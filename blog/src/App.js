a9ed91f82a7d5b5323e5c7d135c1f5f25b4cacc3
zerochae <zerochae@kakao.com>
2021-11-18 01:47:09 +0900

    #React Study 1

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,46 +1,74 @@
 import "./App.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 /* eslint-disable */
 
 function App() {
-
   let posts = "강남 고기 맛집";
-  let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','리액트 마스터']);
-  let [따봉,따봉변경] = useState(0);
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "강남 우동 맛집",
+    "리액트 마스터",
+  ]);
+  let [따봉, 따봉변경] = useState(0);
 
-  function 제목바꾸기(){
-
+  function 제목바꾸기() {
     var newArray = [...글제목];
 
-    newArray[0] = '여자 코트 추천';
+    newArray[0] = "여자 코트 추천";
 
     글제목변경(newArray);
-
   }
-
-
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog </div>
       </div>
-      <button onClick={ ()=>{ 제목바꾸기() } }> 버튼 </button>
+      <button
+        onClick={() => {
+          제목바꾸기();
+        }}
+      >
+        {" "}
+        버튼{" "}
+      </button>
       <div className="list">
-        <h3> { 글제목[0] } <span onClick={ () => {따봉변경(따봉+1)} }>👍</span> {따봉} </h3>
+        <h3>
+          {" "}
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {따봉}{" "}
+        </h3>
         <p> 2월 17일 발행 </p>
         <hr />
       </div>
       <div className="list">
-        <h3> { 글제목[1] }  </h3>
+        <h3> {글제목[1]} </h3>
         <p> 2월 18일 발행 </p>
         <hr />
       </div>
       <div className="list">
-        <h3> { 글제목[2] } </h3>
+        <h3> {글제목[2]} </h3>
         <p> 2월 19일 발행 </p>
         <hr />
       </div>
       <p>기타 내용 </p>
+      <Modal/>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
