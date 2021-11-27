4f973871d3c13eb5f300555b60974226bc881b6e
zerochae <zerochae@kakao.com>
2021-11-27 23:16:13 +0900

    react-router-dom

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,8 +1,10 @@
+/* eslint-disable */
 import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
 import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import data from "./data.js";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
   let [shoes, changeShoe] = useState(data);
@@ -40,28 +42,57 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* 대문  시작*/}
-      <div className="jumbotron">
-        <p>
+      <Route exact path="/">
+        <div>
           {" "}
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero sit
-          blanditiis fugiat inventore consectetur cupiditate maxime explicabo
-          voluptatibus saepe, dolores fugit, veniam aliquid, eligendi accusamus
-          commodi temporibus nobis debitis aut.{" "}
-        </p>
-        <p>
-          {" "}
-          <Button variant="primary"> read more </Button>{" "}
-        </p>
-      </div>
-      {/* 대문 끝 */}
-      <div className="container">
-        <div className="row">
-          {shoes.map((shoe, i) => {
-            return <Item shoe={shoe} i={i} key={i}/>;
-          })}
+          {/* 대문  시작*/}
+          <div className="jumbotron">
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero sit
+              blanditiis fugiat inventore consectetur cupiditate maxime
+              explicabo voluptatibus saepe, dolores fugit, veniam aliquid,
+              eligendi accusamus commodi temporibus nobis debitis aut.{" "}
+            </p>
+            <p>
+              {" "}
+              <Button variant="primary"> read more </Button>{" "}
+            </p>
+          </div>
+          {/* 대문 끝 */}
         </div>
-      </div>
+      </Route>
+      <Route path="/detail">
+        <div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <img
+                  src="https://codingapple1.github.io/shop/shoes1.jpg"
+                  width="100%"
+                />
+              </div>
+              <div className="col-md-6 mt-4">
+                <h4 className="pt-5">상품명</h4>
+                <p>상품설명</p>
+                <p>120000원</p>
+                <button className="btn btn-danger">주문하기</button>
+              </div>
+            </div>
+          </div>{" "}
+        </div>
+      </Route>
+      <Route path="/card">
+        {/* 신발 카드 시작 */}
+        <div className="container">
+          <div className="row">
+            {shoes.map((shoe, i) => {
+              return <Item shoe={shoe} i={i} key={i} />;
+            })}
+          </div>
+        </div>
+        {/* 신발 카드 끝*/}
+      </Route>
     </div>
   );
 }
@@ -70,8 +101,8 @@ function Item(props) {
   return (
     <div className="col-md-4">
       <img
-        src={ `https://codingapple1.github.io/shop/shoes${props.i+1}.jpg` }
-        alt={`shoe${props.i+1}`}
+        src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`}
+        alt={`shoe${props.i + 1}`}
         width="100%"
       />
       <h4> {props.shoe.title} </h4>
